02ef2f55230da2a0466d47f9a2d99ed998037926
Al Viro <viro@zeniv.linux.org.uk>
2020-09-02 11:30:48 -0400

    fix regression in "epoll: Keep a reference on files added to the check list"
    [ Upstream commit 77f4689de17c0887775bb77896f4cc11a39bf848 ]
    
    epoll_loop_check_proc() can run into a file already committed to destruction;
    we can't grab a reference on those and don't need to add them to the set for
    reverse path check anyway.
    
    Tested-by: Marc Zyngier <maz@kernel.org>
    Fixes: a9ed4a6560b8 ("epoll: Keep a reference on files added to the check list")
    Signed-off-by: Al Viro <viro@zeniv.linux.org.uk>
    Signed-off-by: Sasha Levin <sashal@kernel.org>

--- fs/eventpoll.c
+++ fs/eventpoll.c
@@ -1917,7 +1917,6 @@ static int ep_loop_check_proc(void *priv, void *cookie, int call_nests)
 				if (get_file_rcu(epi->ffd.file))
 					list_add(&epi->ffd.file->f_tfile_llink,
 						 &tfile_check_list);
-
 			}
 		}
 	}
